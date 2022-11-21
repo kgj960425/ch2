c06ae3de2a5b1bd60c9f776025193ede3da51583
kgj960425 <kgj960425@gmail.com>
2022-11-21 18:00:16 +0900

    twoDice 클라이언트와 서버

--- ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
+++ ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
@@ -15,6 +15,9 @@ public class TwoDice {
 	public void main(HttpServletResponse response) throws IOException {
 		//랜덤 메소드
 		//int idx1 = (int)(Math.random()*6)+1;
+		//Math.random()는 0~1 사이의 실수를 리턴합니다. (1 미포함) double 사용
+		//Math.random()*6이면 0~6사이의 실수를 리턴합니다. (6 미포함이겠죠?) double 사용하지만 앞에 '(int)'로 형변환을 해주면 아주 좋습니다.
+		//Math.random()*10을 하면 0~ 9.99999...의 범위가 되고 Math.floor(Math.random()*10)의 결과는 0~9범위의 정수입니다. +1을 해준다면 1~10이고 *11을 해준다면 0~10까지의 범위를 얻습니다. 
 		double result1 = Math.random();
 		int result2 = (int)result1;
 		int result3 = result2*6;
