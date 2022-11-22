f577cc9b630f5e90f8af644181f767c5142b6175
kgj960425 <kgj960425@gmail.com>
2022-11-22 16:51:16 +0900

    RequestHeader

--- ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
+++ ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
@@ -19,13 +19,13 @@ public class TwoDice {
 		//Math.random()*6이면 0~6사이의 실수를 리턴합니다. (6 미포함이겠죠?) double 사용하지만 앞에 '(int)'로 형변환을 해주면 아주 좋습니다.
 		//Math.random()*10을 하면 0~ 9.99999...의 범위가 되고 Math.floor(Math.random()*10)의 결과는 0~9범위의 정수입니다. +1을 해준다면 1~10이고 *11을 해준다면 0~10까지의 범위를 얻습니다. 
 		double result1 = Math.random();
-		int result2 = (int)result1;
-		int result3 = result2*6;
+		double result2 = result1*6;
+		int result3 = (int)result2;
 		int idx1 = result3+1;
 		//int idx2 = (int)(Math.random()*6)+1;
 		double result4 = Math.random();
-		int result5 = (int)result4;
-		int result6 = result5*6;
+		double result5 = result4*6;
+		int result6 = (int)result5;
 		int idx2 = result6+1;
 				
 		response.setContentType("text/html");
